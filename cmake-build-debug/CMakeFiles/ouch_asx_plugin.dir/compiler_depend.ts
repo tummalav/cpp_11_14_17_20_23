b930b99141273e6e0a590c9336a5add9aa9eba2a
Tummala Venkata Sateesh <tummalavenkatasateesh@Tummalas-MacBook-Air.local>
2026-04-03 13:04:07 +0800

    chore: remove cmake-build-debug build artifacts from tracking

--- cmake-build-debug/CMakeFiles/ouch_asx_plugin.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for ouch_asx_plugin.
--- cmake-build-debug/CMakeFiles/ouch_asx_plugin.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ouch_asx_plugin.
-# This may be replaced when dependencies are built.
